233c941a77e446deccd5a1364da659b53a9917cd
S. Steensma <ssteensma@oacsd.com>
2024-03-20 15:07:31 -0400

    Removed Sim stuff. Cleaned up code.

--- src/main/java/frc/robot/Subsystems/SubDrive.java
+++ src/main/java/frc/robot/Subsystems/SubDrive.java
@@ -4,7 +4,6 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.util.PathPlannerLogging;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
@@ -15,27 +14,23 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Config.Constants;
+import frc.robot.Config.Ports.pSwerve;
+import frc.robot.Sensor.Navigation;
 import frc.robot.Sensor.Selector;
 
 public class SubDrive extends SubsystemBase {
-  private SimSwerveModule[]     modules;
-  private SwerveDriveKinematics kinematics;
-  private SwerveDriveOdometry   Odometer;
-
-  private SimGyro
-    gyro;
-  
-  private Field2d
-    Field = new Field2d();
+  public SubSwerve[]           Modules                    ;
+  public SwerveDriveKinematics kinematics                 ;
+  public SwerveDriveOdometry   Odometer                   ;
+  public Field2d               Field      = new Field2d() ;
   
   public SubDrive() {
-    gyro = new SimGyro();
 
-    modules = new SimSwerveModule[]{
-      new SimSwerveModule(),
-      new SimSwerveModule(),
-      new SimSwerveModule(),
-      new SimSwerveModule()
+    Modules = new SubSwerve[]{
+      new SubSwerve( pSwerve.CAN_FL ),
+      new SubSwerve( pSwerve.CAN_FR ),
+      new SubSwerve( pSwerve.CAN_BL ),
+      new SubSwerve( pSwerve.CAN_BR )
     };
 
     kinematics = new SwerveDriveKinematics(
@@ -47,7 +42,7 @@ public class SubDrive extends SubsystemBase {
 
     Odometer = new SwerveDriveOdometry(
       kinematics,
-      gyro.getRotation2d(),
+      Navigation.NavX.getRotation2d(),
       getPositions()
     );
 
@@ -67,9 +62,7 @@ public class SubDrive extends SubsystemBase {
   }
 
   @Override public void periodic() {
-    // Update the simulated gyro, not needed in a real project
-    gyro.updateRotation( getSpeeds().omegaRadiansPerSecond );
-    Odometer .update( gyro.getRotation2d(), getPositions() );
+    Odometer .update( Navigation.NavX.getRotation2d(), getPositions() );
     Field    .setRobotPose( getPose() );
   }
 
@@ -78,7 +71,7 @@ public class SubDrive extends SubsystemBase {
   }
 
   public void resetPose( Pose2d pose ) {
-    Odometer.resetPosition( gyro.getRotation2d(), getPositions(), pose );
+    Odometer.resetPosition( Navigation.NavX.getRotation2d(), getPositions(), pose );
   }
 
   public ChassisSpeeds getSpeeds() {
@@ -94,7 +87,7 @@ public class SubDrive extends SubsystemBase {
   public void     RobotDrive( double X, double Y, double Z ) { driveRobotRelative( new ChassisSpeeds( X, Y, Z ) ); }
   public Command cRobotDrive( double X, double Y, double Z ) { return this.runOnce( () -> RobotDrive( X, Y, Z ) ); }
 
-  
+
   public void driveFieldRelative( ChassisSpeeds FieldSpeeds ) {
     driveRobotRelative( ChassisSpeeds.fromFieldRelativeSpeeds( FieldSpeeds, getPose().getRotation() ) );
   }
@@ -107,57 +100,19 @@ public class SubDrive extends SubsystemBase {
 
   public void setStates(SwerveModuleState[] targetStates) {
     SwerveDriveKinematics.desaturateWheelSpeeds( targetStates, Constants.Swerve.maxModuleSpeed );
-    for (int i = 0; i < modules.length; i++) { modules[i].setTargetState( targetStates[i] ); }
+    for ( int i = 0; i < Modules.length; i++ ) { Modules[i].setTargetState( targetStates[i] ); }
   }
 
   public SwerveModuleState[] getModuleStates() {
-    SwerveModuleState[] states = new SwerveModuleState[modules.length];
-    for ( int i = 0; i < modules.length; i++ ) { states[i] = modules[i].getState(); }
+    SwerveModuleState[] states = new SwerveModuleState[ Modules.length];
+    for ( int i = 0; i < Modules.length; i++ ) { states[i] = Modules[i].getState(); }
     return states;
   }
 
   public SwerveModulePosition[] getPositions() {
-    SwerveModulePosition[] positions = new SwerveModulePosition[ modules.length ];
-    for ( int i = 0; i < modules.length; i++ ) { positions[i] = modules[i].getPosition(); }
+    SwerveModulePosition[] positions = new SwerveModulePosition[ Modules.length ];
+    for ( int i = 0; i < Modules.length; i++ ) { positions[i] = Modules[i].getPosition(); }
     return positions;
   }
 
-  /**
-   * Basic simulation of a swerve module, will just hold its current state and not use any hardware
-   */
-  class SimSwerveModule {
-    private SwerveModulePosition currentPosition = new SwerveModulePosition ();
-    private SwerveModuleState    currentState    = new SwerveModuleState    ();
-
-    public SwerveModulePosition getPosition () { return currentPosition; }
-    public SwerveModuleState    getState    () { return currentState;    }
-
-    public void setTargetState(SwerveModuleState targetState) {
-      // Optimize the state
-      currentState = SwerveModuleState.optimize(
-        targetState,
-        currentState.angle
-      );
-
-      currentPosition = new SwerveModulePosition(
-        currentPosition.distanceMeters + ( currentState.speedMetersPerSecond * 0.02 ),
-        currentState.angle
-      );
-    }
-  }
-
-  /**
-   * Basic simulation of a gyro, will just hold its current state and not use any hardware
-   */
-  class SimGyro {
-    private Rotation2d currentRotation = new Rotation2d();
-
-    public Rotation2d getRotation2d() {
-      return currentRotation;
-    }
-
-    public void updateRotation( double angularVelRps ) {
-      currentRotation = currentRotation.plus( new Rotation2d( angularVelRps * 0.02 ) );
-    }
-  }
 }
--- src/main/java/frc/robot/Subsystems/SubSwerve.java
+++ src/main/java/frc/robot/Subsystems/SubSwerve.java
@@ -1,7 +1,13 @@
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SubSwerve extends SubsystemBase {
@@ -10,11 +16,61 @@ public class SubSwerve extends SubsystemBase {
       Drive,
       Steer;
 
+    public CANcoder
+      Encod;
+
     public SubSwerve( int[] ID ) {
-      Drive = new TalonFX( ID[0] );
-      Steer = new TalonFX( ID[1] );
+      Drive = new TalonFX ( ID[0] );
+      Steer = new TalonFX ( ID[1] );
+      Encod = new CANcoder( ID[2] );
+
+      final PositionVoltage m_voltagePosition = new PositionVoltage( 0, 0, true, 0, 0, false, false, false ); // Slot0
+      final VelocityVoltage m_voltageVelocity = new VelocityVoltage( 0, 0, true, 0, 1, false, false, false ); // Slot1
+
+      TalonFXConfiguration configs = new TalonFXConfiguration();
+      
+      /* VOLTAGE BASED VELOCITY */
+      configs.Slot0.kP = 0.10; // An error of 1 rev/sec => 2V output
+      configs.Slot0.kI = 0.00; //
+      configs.Slot0.kD = 0.00; //
+      configs.Slot0.kV = 0.12; //
+
+      /* POSITION CONTROL */
+      configs.Slot1.kP = 0.10; //
+      configs.Slot1.kI = 0.00; //
+      configs.Slot1.kD = 0.00; //
+      configs.Slot1.kV = 0.05; //
+
+      configs.CurrentLimits.StatorCurrentLimit       = 40;
+      configs.CurrentLimits.StatorCurrentLimitEnable = true;
+
+      configs.Voltage.PeakForwardVoltage =  8;
+      configs.Voltage.PeakReverseVoltage = -8;
+
+      // configs.TorqueCurrent.PeakForwardTorqueCurrent =  40;
+      // configs.TorqueCurrent.PeakReverseTorqueCurrent = -40;
+
+      Drive.getConfigurator().apply( configs );
+      Steer.getConfigurator().apply( configs );
     }
 
-  @Override public void periodic() {}
+  @Override public void periodic() {
+    // Drive.setControl( null );
+    // Steer.setControl( null );
+  }
+
+  private SwerveModulePosition currentPosition = new SwerveModulePosition ();
+  private SwerveModuleState    currentState    = new SwerveModuleState    ();
+
+  public SwerveModulePosition getPosition () { return currentPosition; }
+  public SwerveModuleState    getState    () { return currentState;    }
+
+  public void setTargetState( SwerveModuleState targetState ) {
+    currentState = SwerveModuleState.optimize( targetState, currentState.angle );
+    currentPosition = new SwerveModulePosition(
+      currentPosition.distanceMeters + ( currentState.speedMetersPerSecond * 0.02 ),
+      currentState.angle
+    );
+  }
 
 }
