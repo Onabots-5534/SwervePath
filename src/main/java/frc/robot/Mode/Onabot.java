a56b7899679361b9021d0192b4286ac920165e13
S. Steensma <ssteensma@oacsd.com>
2024-03-22 13:32:48 -0400

    Added ShuffleboardTab definitions to Onabot.

--- src/main/java/frc/robot/Mode/Onabot.java
+++ src/main/java/frc/robot/Mode/Onabot.java
@@ -1,10 +1,17 @@
 package frc.robot.Mode;
 
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Robot;
 import frc.robot.RobotContainer;
 
 public class Onabot {
+
+    public static ShuffleboardTab
+        Auto = Shuffleboard.getTab( "Auto" ),
+        Comp = Shuffleboard.getTab( "Comp" ),
+        Test = Shuffleboard.getTab( "Test" );
 
     public static void Initialize() {
         Robot.m_Container = new RobotContainer();
