eb66f2f6076c413ab5b7e5d5481ecad73fd91512
S. Steensma <ssteensma@oacsd.com>
2024-03-29 20:29:05 -0400

    Cleanup

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -69,7 +69,6 @@ public static void Shuffle() {
     // Add a button to run the example auto to SmartDashboard, this will also be in the auto chooser built above
     // SmartDashboard.putData( "Example Auto", new PathPlannerAuto("Example Auto") );
     // SmartDashboard.putData( "Ctr-C",        new PathPlannerAuto( "Ctr-C" )      );
-    // SmartDashboard.putData( "Ctr-C",        new PathPlannerAuto( "Ctr-C" )      );
 
     // Add a button to run pathfinding commands to SmartDashboard
     SmartDashboard.putData( "Pickup Position",  NewPose.Absolute( 14.00, 6.50, 0 ) );
