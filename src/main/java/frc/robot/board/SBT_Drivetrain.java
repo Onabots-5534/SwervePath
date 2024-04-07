19b5afb22f21a4d74fc3b3a0c192a3e0214ad801
= <=>
2024-04-07 11:21:08 -0400

    Correct some errors in SBT Drivetrain

--- src/main/java/frc/robot/board/SBT_Drivetrain.java
+++ src/main/java/frc/robot/board/SBT_Drivetrain.java
@@ -12,9 +12,9 @@ public class SBT_Drivetrain {
 
     public static GenericEntry
         FL_PV = SBT.add("FL PV",0).withPosition( 5, 0 ).withSize( 2, 1 ).getEntry(),
-        FL_SP = SBT.add("FL SP",0).withPosition( 5, 0 ).withSize( 2, 1 ).getEntry();
+        FL_SP = SBT.add("FL SP",0).withPosition( 5, 0 ).withSize( 2, 1 ).getEntry(),
 
-        Auton_Scale = SBT.addPersistent("Auton Scale",0).withPosition( 7, 1 ).withSize( 2, 1 ).getEntry;
+        Auton_Scale = SBT.addPersistent("Auton Scale",0).withPosition( 7, 1 ).withSize( 2, 1 ).getEntry();
 
     public static void Initialize() {
     }
